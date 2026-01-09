7aa7619bae0a42ea664cd849f9a3457148ae4ce1
jacob <jacobappleid2@gmail.com>
2026-01-09 01:01:25 +0100

    första enkla version av inloggscript

---
+++ login_audit.sh
@@ -0,0 +1,16 @@
+
+
+
+#!/bin/bash
+
+# simpelt inlogg script
+# visar grundläggande info om systemet
+
+echo "Inloggningskontroll"
+echo "-------------------"
+
+date          # Visar datom och tid
+whoami        # Visar vilken användare som är inloggad
+hostname      # Visar datorns namn
+
+
