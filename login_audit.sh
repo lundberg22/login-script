90faba5f6dec79c2e6fac738644e91219b8ca193
elinkrell <elinkrell1@gmail.com>
2026-01-11 10:32:20 +0100

    PR/fixa typos

--- login_audit.sh
+++ login_audit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# enkelt inloggscript
+# enkelt inloggsscript
 LOGG_FIL="login_logg.txt"
 
 #echo "inloggningskontroll" | tee "$LOGG_FIL"
@@ -8,7 +8,7 @@ date | tee -a "$LOGG_FIL"     # datum och tid
 whoami | tee -a "$LOGG_FIL"   # användare
 hostname | tee -a "$LOGG_FIL" # datornamn
 echo "" | tee -a "$LOGG_FIL"
-uptime | tee -a "$LOGG_FIL"   # hur länge systemet vairt igång
+uptime | tee -a "$LOGG_FIL"   # hur länge systemet varit igång
 echo "" | tee -a "$LOGG_FIL"
 echo "Disk (df -h):" | tee -a "$LOGG_FIL"
 df -h | tee -a "$LOGG_FIL"
